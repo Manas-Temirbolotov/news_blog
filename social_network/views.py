1232ecaaa13d3570f6e38fecd858a9e330feb4ea
Manas-Temirbolotov <temirbolotovmanas@gmail.com>
2022-12-21 18:45:09 +0600

    authentication and permission

--- social_network/views.py
+++ social_network/views.py
@@ -1,13 +1,18 @@
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication, TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, BasePermission, SAFE_METHODS
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from .permissions import ReadOnlyPermission, IsAuthorPermission
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthorPermission, ]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -31,3 +36,12 @@ class CommentListCreateView(ListCreateAPIView):
 class CommentRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthorPermission, ]
+
+
+class IsAuthenticatedOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_authenticated)
